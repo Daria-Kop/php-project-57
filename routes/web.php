ad72604b63d7521482680d30dec1d46c015158e5
user <kstdasha2013@gmail.com>
2025-03-07 23:11:07 +0300

    otkat

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,36 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::resource('task_statuses', TaskStatusController::class);
+Route::resource('task_statuses', TaskStatusController::class)
+    ->names([
+        'store' => 'status.create',
+        'create' => 'status.build',
+        'edit' => 'status.edit',
+        'update' => 'status.update',
+        'destroy' => 'status.destroy',
+        'index' => 'status.index',
+    ]);
 
-Route::resource('tasks', TaskController::class);
+Route::resource('tasks', TaskController::class)
+    ->names([
+        'store' => 'task.create',
+        'create' => 'task.build',
+        'edit' => 'task.edit',
+        'update' => 'task.update',
+        'destroy' => 'task.destroy',
+        'index' => 'task.index',
+        'show' => 'task.show',
+    ]);
 
-Route::resource('labels', LabelController::class);
+Route::resource('labels', LabelController::class)
+    ->names([
+        'store' => 'label.create',
+        'create' => 'label.build',
+        'edit' => 'label.edit',
+        'update' => 'label.update',
+        'destroy' => 'label.destroy',
+        'index' => 'label.index',
+        'show' => 'label.show',
+    ]);
 
 require __DIR__ . '/auth.php';
